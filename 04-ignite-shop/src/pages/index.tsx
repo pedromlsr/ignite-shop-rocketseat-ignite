93a7395abf0fc7a4bb3057a56ba12d0f00305db5
pedromlsr <pedrodesarodrigues@gmail.com>
2023-12-14 23:25:51 -0300

    getServerSideProps example

--- 04-ignite-shop/src/pages/index.tsx
+++ 04-ignite-shop/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import Image from "next/image"
 import { HomeContainer, Product } from "../styles/pages/home"
 import { useKeenSlider } from 'keen-slider/react'
@@ -8,7 +9,7 @@ import shirt3 from '../assets/shirts/3.png'
 
 import 'keen-slider/keen-slider.min.css'
 
-export default function Home() {
+export default function Home(props: any) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -18,6 +19,7 @@ export default function Home() {
 
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
       <Product className="keen-slider__slide">
         <Image src={shirt1} width={520} height={480} alt="" />
 
@@ -55,4 +57,16 @@ export default function Home() {
       </Product>
     </HomeContainer>
   )
+}
+
+export const getServerSideProps = async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000))
+  
+  console.log('rodou')
+
+  return {
+    props: {
+      list: [1, 2, 3]
+    }
+  }
 }
