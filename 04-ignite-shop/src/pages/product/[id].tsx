94277211fe6af04aedc4fb2969a33ff831d9fa52
pedromlsr <pedrodesarodrigues@gmail.com>
2023-12-16 00:19:00 -0300

    product page style

--- 04-ignite-shop/src/pages/product/[id].tsx
+++ 04-ignite-shop/src/pages/product/[id].tsx
@@ -1,9 +1,22 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const { query } = useRouter()
 
   return (
-    <h1>Product: {JSON.stringify(query)}</h1>
-  )
+      <ProductContainer>
+        <ImageContainer>
+        </ImageContainer>
+        
+        <ProductDetails>
+          <h1>Camiseta X</h1>
+          <span>R$ 79,90</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error blanditiis earum modi dolore quam quas distinctio nihil ipsa molestiae? Possimus quia sit corporis laboriosam. Nulla quisquam suscipit incidunt accusamus ipsam?</p>
+          <button>
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    )
 }
