fe3dcaeb32527516fcd4bc35cc2605202bf436fa
pedromlsr <pedrodesarodrigues@gmail.com>
2023-12-16 20:17:06 -0300

    success page styling

--- 04-ignite-shop/src/pages/success.tsx
+++ 04-ignite-shop/src/pages/success.tsx
@@ -1,5 +1,23 @@
-export default function Sroduct() {
+import Link from "next/link";
+// import Image from "next/image";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
+export default function Success() {
   return (
-    <h1>Success</h1>
+      <SuccessContainer>
+        <h1>Compra efetuada</h1>
+
+        <ImageContainer>
+          
+        </ImageContainer>
+
+        <p>
+          Uhuul <strong>Pedro Rodrigues</strong>, sua <strong>Camiseta</strong> já está a caminho da sua casa.
+        </p>
+
+        <Link href="/">
+          Voltar ao catálogo
+        </Link>
+      </SuccessContainer>
   )
 }
